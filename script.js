852e2263d6c9389e8382d3492fbd27cbe34f5eea
Eligio93 <eligio.cristantielli@gmail.com>
2023-04-12 10:16:09 +1000

    add grid to the main container

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let dimension=0;
+let flexBasis="";
 
 const button=document.querySelector("button").addEventListener("click",function(e){
     dimension=document.querySelector('#gridsize').value;
-    console.log(dimension);
+    const container =document.getElementById("container");
+    //set the dimension of the div removing the border
+    flexBasis=((500/dimension)-2)+"px";
+    //every click remove the previous grid
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    //loop that creates the div and append them to the container
+    for(let i=0;i<(dimension*dimension);i++){
+        const div=document.createElement("div");
+        div.classList.add("box");
+        div.style.flexBasis=flexBasis;
+        div.style.flexGrow=1;
+        div.style.flexShrink=1;        
+        container.appendChild(div);
+    }
+
   });
