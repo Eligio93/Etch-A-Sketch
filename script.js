7f0335abeb2e40db5a0fe930d76386fabe55ac01
Eligio93 <eligio.cristantielli@gmail.com>
2023-04-12 23:55:52 +1000

    add a resetGrid Function to perform all task

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 let flexBasis="";
 let color="#00FF00";
-
-const button=document.getElementById("btndimension").addEventListener("click",function(e){
+function randomColor() {
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + "0".repeat(6 - color.length) + color;
+  }
+  function resetGrid(){
     const dimension=document.querySelector('#gridsize').value;
     const container =document.getElementById("container");
-    //set the dimension of the div removing the border
     flexBasis=((500/dimension)-2)+"px";
-    //every click remove the previous grid
     while(container.firstChild){
         container.removeChild(container.firstChild);
+        
     }
-    //loop that creates the div and append them to the container
     for(let i=0;i<(dimension*dimension);i++){
         const div=document.createElement("div");
         div.classList.add("box");
         div.style.flexBasis=flexBasis;
         div.style.flexGrow=1;
-        div.style.flexShrink=0;        
+        div.style.flexShrink=0;  
+        div.style.backgroundColor="blue";      
         container.appendChild(div);
      }
-     //applcia la funzione di mouseover e cambia colore
-     //il colore inizialmente è quello dichiarato all'inziio dello script
-    const over = document.querySelectorAll(".box");
+     const over = document.querySelectorAll(".box");
     over.forEach(box => {
     box.addEventListener('mouseover', function() {
     this.style.backgroundColor = color;
   });
 });
 
-   
+  }
+
+const button=document.getElementById("btndimension").addEventListener("click",function(e){
+    resetGrid();
+
  });
- //Seleziona il colore desiderato
+ //Seleziona il colore desiderato ed è stata riapplicata per tutti i div in quanto non si poteva tornare
+ //da unicorn a selezionare il colore
  const btncolor=document.getElementById("btncolor").addEventListener("click",function(e){
     color=document.getElementById("colorpicker").value;
-   
-  
+    document.querySelectorAll(".box").forEach(function(div){
+        div.addEventListener("mouseover",function(){
+            this.style.backgroundColor=color;
+        })
+    })
+    });
+//implementata funzione per assegnare colori random ai diversi div
+    const btnUnicorn=document.getElementById("unicorn").addEventListener("click", function(el){
+        document.querySelectorAll(".box").forEach(function(unidiv){
+            unidiv.addEventListener("mouseover",function(){
+                this.style.backgroundColor=randomColor();
+            })
+        })
+    });
+    const clear=document.getElementById("clear").addEventListener("click", function(){
+        resetGrid();
+        });
     
-});
 
 
